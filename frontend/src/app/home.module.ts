59de2f4208e972525a18f29ff942fb27a2379fe7
adrian61625 <adrian61625@mastek.com>
2021-12-01 20:21:11 +0000

    Commit
    Added registration component with a card form with all user details on it

--- frontend/src/app/home.module.ts
+++ frontend/src/app/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { homeComponent } from './home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [homeComponent],
+  declarations: [homeComponent, RegistrationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [homeComponent],

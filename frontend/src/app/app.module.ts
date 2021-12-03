34b0fe5000ef5af85669bfcd4f361115760e128a
Zachary Death <Zachary.death@hotmail.co.uk>
2021-12-03 10:55:10 +0000

    changed home back to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { homeComponent } from './home.component';
+import { appComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [homeComponent, RegistrationComponent],
+  declarations: [appComponent, RegistrationComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [homeComponent],
+  bootstrap: [appComponent],
 })
-export class homeModule {}
+export class appModule {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { homeModule } from './app/home.module';
+import { appModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(homeModule)
+platformBrowserDynamic().bootstrapModule(appModule)
   .catch(err => console.error(err));

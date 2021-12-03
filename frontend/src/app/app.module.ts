255d4647b6f354475e6ec4adf54e50b423677f93
Zachary Death <Zachary.death@hotmail.co.uk>
2021-12-03 11:03:15 +0000

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { appComponent } from './app.component';
-// import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [appComponent],

71ffdcb0b4a6d5f39341297313992bb72cf36026
Zachary Death <Zachary.death@hotmail.co.uk>
2021-12-03 11:02:07 +0000

    fixed?

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { appComponent } from './app.component';
-import { RegistrationComponent } from './components/registration/registration.component';
+// import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [appComponent, RegistrationComponent],
+  declarations: [appComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [appComponent],

8e27649e438df438d8af1bb31b6f8a4e40cc38eb
Zachary Death <Zachary.death@hotmail.co.uk>
2021-12-07 14:14:06 +0000

    merged routing modules

--- frontend/src/app/routing.module.ts
+++ frontend/src/app/routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 //configure route paths
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'home', component: MainPageComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: '**', component: MainPageComponent },
+];
 
 @NgModule({
   imports: [CommonModule,

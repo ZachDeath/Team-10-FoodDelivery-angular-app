4975da1964bd91dd2259571177197f986c5e7f44
Zachary Death <Zachary.death@hotmail.co.uk>
2021-12-22 16:44:14 +0000

    changed basket to display price
    kinda ugly right now but it works

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from 'src/app/services/basket.service';
+import { menuItem } from '../../shared/menuItem.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  itemsInBasket: menuItem[] = [];
+  basketPrice: number = 0;
+  imagePath = 'assets/images/pizza.png';
 
-  imagePath = "assets/images/pizza.png"
-
-  constructor() { 
-    console.log("Header Loaded")
+  constructor(private bService: BasketService) {
+    console.log('Header Loaded');
   }
 
   ngOnInit(): void {
+    this.itemsInBasket = this.bService.getItems();
+    this.totalPrice();
+    this.bService.itemsChanged.subscribe((items: menuItem[]) => {
+      this.itemsInBasket = items;
+      this.totalPrice();
+    });
   }
 
+  totalPrice() {
+    let sum = 0;
+    this.itemsInBasket.forEach((element) => {
+      sum += element.price * element.quantity;
+    });
+
+    this.basketPrice = sum;
+  }
 }

6144e6983e7a6c986be7283bbb471d485247f6e5
smithy08 <93933969+smithy08@users.noreply.github.com>
2022-01-26 19:28:21 +0000

    more frontend improvements

--- frontend/src/app/components/account-components/account-address/account-address.component.ts
+++ frontend/src/app/components/account-components/account-address/account-address.component.ts
@@ -14,6 +14,13 @@ export class AccountAddressComponent implements OnInit {
 
   @ViewChild('addressForm') addressForm: NgForm;
 
+  firstname: String;
+  lastname:String;
+  line1: String;
+  line2: String;
+  city: String;
+  postcode: String;
+
   constructor() { }
 
   
@@ -23,6 +30,22 @@ export class AccountAddressComponent implements OnInit {
 
   onSubmit(): void {
 
+    console.log(this.addressForm.value.firstname);
+    console.log(this.addressForm.value.lastname);
+    console.log(this.addressForm.value.address1);
+    console.log(this.addressForm.value.address2);
+    console.log(this.addressForm.value.city);
+    console.log(this.addressForm.value.state);
+    console.log(this.addressForm.value.post);
+    
+    this.clearForm();
+
+  }
+
+  clearForm(){
+
+    this.addressForm.form.reset();
+    
   }
 
   

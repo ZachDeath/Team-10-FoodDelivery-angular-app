6717d70ba6302fa0c3855d161026dddb22a28530
Zachary Death <Zachary.death@hotmail.co.uk>
2022-01-27 12:43:37 +0000

    started intergrating backend and frontend

--- frontend/src/app/components/admin/overview/overview.component.ts
+++ frontend/src/app/components/admin/overview/overview.component.ts
@@ -1,15 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+  styleUrls: ['./overview.component.css'],
 })
 export class OverviewComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http.get('http://localhost:8090/users/getUsers').subscribe((users) => {
+     console.log(users);
+    });
   }
-
 }

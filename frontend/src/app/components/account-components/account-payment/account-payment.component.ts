b6ebe7ed616b0560ed99ca80497f747b82ed0de4
smithy08 <93933969+smithy08@users.noreply.github.com>
2022-02-04 17:36:25 +0000

    more changes recovered - can view payment and almost update

--- frontend/src/app/components/account-components/account-payment/account-payment.component.ts
+++ frontend/src/app/components/account-components/account-payment/account-payment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { PaymentService } from 'src/app/services/payment.service';
+import { UserService } from 'src/app/services/user.service';
+import { Payment } from 'src/app/shared/paymentConstructor';
 
 
 
@@ -13,7 +16,7 @@ export class AccountPaymentComponent implements OnInit {
 
   @ViewChild('paymentForm') paymentForm: NgForm;
 
-  constructor() { }
+  constructor(private userservice: UserService, private paymentService: PaymentService) { }
 
   
   cardNumberGroup: FormGroup;
@@ -22,10 +25,13 @@ export class AccountPaymentComponent implements OnInit {
   }
 
   onSubmit():void{
-    console.log(this.paymentForm.value.fullname);
-    console.log(this.paymentForm.value.cardnumber);
-    console.log(this.paymentForm.value.expiry);
-    console.log(this.paymentForm.value.cvv);
+    //console.log(this.paymentForm.value)
+    let temp = new Payment(this.userservice.userObj.user_id,this.paymentForm.value.cardType,this.paymentForm.value.cardnumber,
+      this.paymentForm.value.vcc,this.paymentForm.value.expiryM,this.paymentForm.value.expiryY);
+
+    //console.log(temp);
+
+      this.paymentService.updatePayment(temp);
 
     this.clearForm();
   }

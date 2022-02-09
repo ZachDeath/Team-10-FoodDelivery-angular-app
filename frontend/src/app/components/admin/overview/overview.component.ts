34da9cd5e667841ac65e6e3729c8c2eadf050086
adrian61625 <adrian61625@mastek.com>
2022-02-09 19:06:01 +0000

    new changes on admin

--- frontend/src/app/components/admin/overview/overview.component.ts
+++ frontend/src/app/components/admin/overview/overview.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmployeeService } from 'src/app/services/employee.service';
 import { MessageService } from 'src/app/services/message.service';
+import { OrderService } from 'src/app/services/order.service';
 import { UserService } from 'src/app/services/user.service';
 import { Employee } from 'src/app/shared/employeeConstructor';
 import { Message } from 'src/app/shared/messageConstructor';
 import { User } from 'src/app/shared/userConstructor';
+import { Order } from 'src/app/shared/orderConstructor';
 
 
 @Component({
@@ -18,11 +20,14 @@ export class OverviewComponent implements OnInit {
   users: User[];
   messages: Message[];
   loggedAdmin: Employee;
+  orders: Order[];
+  
 
   constructor(private userService: UserService, private messageService: MessageService,
-    private eService: EmployeeService) {
+    private eService: EmployeeService, private oService: OrderService) {
     this.users = [];
     this.messages = [];
+    this.orders = [];
     this.loggedAdmin=this.eService.employeeObj;
   }
 
@@ -32,6 +37,9 @@ export class OverviewComponent implements OnInit {
     });
     this.messageService.getAllMessages().subscribe((messages: Message[]) => {
       this.messages = messages;
+    });
+    this.oService.getAllOrders().subscribe((orders: Order[]) => {
+      this.orders = orders;
     });
   }
 }

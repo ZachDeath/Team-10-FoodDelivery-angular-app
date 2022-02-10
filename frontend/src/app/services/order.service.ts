841eeb55639bb63cacdf7b71cae7cb1aa321a6b8
Zachary Death <Zachary.death@hotmail.co.uk>
2022-02-10 01:58:09 +0000

    added a new orderConstructor as the old one has additonal parameters now

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { CreateOrder } from '../shared/createOrderConstructor';
 import { Order, OrderAdapter } from '../shared/orderConstructor';
 import { BasketService } from './basket.service';
 import { OrderDetailsService } from './orderDetails.service';
-
 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
   private apiUrl = 'http://localhost:8090/api/order';
 
-  constructor(private http: HttpClient, private adapter:OrderAdapter, private orderDetailsService: OrderDetailsService, private basketService: BasketService) {}
+  constructor(
+    private http: HttpClient,
+    private adapter: OrderAdapter,
+    private orderDetailsService: OrderDetailsService,
+    private basketService: BasketService
+  ) {}
 
   //Gets every order stored in the table
   getAllOrders(): Observable<Order[]> {
@@ -21,7 +26,7 @@ export class OrderService {
   }
 
   //Gets every order under a paticular ID in the table
-  getOrdersByUserID(id:number): Observable<Order[]> {
+  getOrdersByUserID(id: number): Observable<Order[]> {
     const url = `${this.apiUrl}/getOrderByUser/${id}`;
     return this.http
       .get(url)
@@ -29,28 +34,25 @@ export class OrderService {
   }
 
   //Gets every order under a paticular ID in the table
-  createNewOrder(id:number, e_id:number): void {
+  createNewOrder(id: number, e_id: number): void {
     const url = `${this.apiUrl}/createOrder/${id}`;
-    let temp = new Order(100, id,e_id,22);
+    let temp = new CreateOrder(null, id, e_id, null);
     this.http
       .post(url, temp)
-      .pipe().subscribe((order:Order)=>{
-
-        for (let i=0;i<this.basketService.itemsInBasket.length;i++){
-          this.orderDetailsService.createNewOrderDetails(order.id,this.basketService.itemsInBasket[i].food_id,this.basketService.itemsInBasket[i].quantity)
-  
+      .pipe()
+      .subscribe((order: CreateOrder) => {
+        for (let i = 0; i < this.basketService.itemsInBasket.length; i++) {
+          this.orderDetailsService.createNewOrderDetails(
+            order.id,
+            this.basketService.itemsInBasket[i].food_id,
+            this.basketService.itemsInBasket[i].quantity
+          );
         }
 
-        this.basketService.itemsInBasket=[];
-        this.basketService.itemsChanged.emit(this.basketService.itemsInBasket.slice());
+        this.basketService.itemsInBasket = [];
+        this.basketService.itemsChanged.emit(
+          this.basketService.itemsInBasket.slice()
+        );
       });
-
-      
-
   }
-
-  
-
-
-
 }
---
+++ frontend/src/app/shared/createOrderConstructor.ts
@@ -0,0 +1,31 @@
+import { Injectable, Optional } from '@angular/core';
+
+import { Adapter } from '../services/adapter';
+
+
+export class CreateOrder {
+  constructor(
+    public id: number,
+
+    public user_id: number,
+
+    public employee_id: number,
+
+    public order_date: number,
+
+  ) {}
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreateOrderAdapter implements Adapter<CreateOrder> {
+  adapt(item: any): CreateOrder {
+    return new CreateOrder(
+      item.id,
+      item.user_id,
+      item.employee_id,
+      item.order_date,
+    );
+  }
+}
